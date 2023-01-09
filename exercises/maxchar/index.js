543a4e6022f8f51f6725daa3afd08a15be4ee898
EMRE MUTLU <mutluemre93@gmail.com>
2023-01-09 19:43:06 +0300

    Test: maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // Check how many times a char repeat
+  if (str.length === 1) return str;
+  const strArr = str.split("");
+  let countedObj = {};
+  strArr.reduce((prev, curr, index) => {
+    if (countedObj[prev]) countedObj[prev]++;
+    else countedObj[prev] = 1;
+    if (strArr.length - 1 === index) {
+      if (countedObj[curr]) countedObj[curr]++;
+      else countedObj[curr] = 1;
+    }
+    return curr;
+  });
+
+  return Object.entries(countedObj).sort((a, b) => b[1] - a[1])?.[0]?.[0];
+}
 
 module.exports = maxChar;
