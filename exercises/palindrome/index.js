e88587a369edb7f3c77b8d98bb6955a1c104ebf0
EMRE MUTLU <mutluemre93@gmail.com>
2023-01-09 12:53:06 +0300

    New: palindrome example is completed!

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str
+    .split("")
+    .every((char, index, arr) => char === arr[arr.length - 1 - index]);
+}
+
+// function palindrome(str) {
+//     return str === str.split("").reverse().join("");
+//   }
 
 module.exports = palindrome;
