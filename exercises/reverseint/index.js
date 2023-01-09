abe4977623edaf74a2e2b40c9cb0324db16e3041
EMRE MUTLU <mutluemre93@gmail.com>
2023-01-09 13:31:51 +0300

    New: reverseInt example is completed!

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const numberSign = Math.sign(n);
+  let newNumber = n * numberSign;
+
+  const numberAsString = String(newNumber);
+  const numberAsArray = numberAsString.split("");
+
+  // if the last elements of number as array is equal "0", remove it, until you find different number
+  const reversedNumberArray = numberAsArray.reverse();
+  let numberArray = [...reversedNumberArray];
+
+  for (let i = 0; i < numberArray.length; i++) {
+    if (reversedNumberArray[i] === "0") {
+      numberArray = [...reversedNumberArray];
+    } else {
+      break;
+    }
+  }
+
+  const resultNumber = parseInt(numberArray.join(""));
+
+  return resultNumber * numberSign;
+}
 
 module.exports = reverseInt;
