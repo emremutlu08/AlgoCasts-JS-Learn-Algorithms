8d53399659e72672f78a2dc07d6542799de1e3d8
emremutlu08 <mutluemre93@gmail.com>
2023-01-15 22:12:29 +0300

    Todo: chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,13 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunkedArray = [...array];
+  chunkedArray = chunkedArray.reduce((prev, curr, index, arr) => {
+    return prev;
+  });
+
+  return chunkedArray;
+}
 
 module.exports = chunk;
