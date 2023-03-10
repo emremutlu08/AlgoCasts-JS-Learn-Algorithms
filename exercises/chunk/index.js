70b1b2ea1ab2b603507ce9c60f625913c22282af
emremutlu08 <mutluemre93@gmail.com>
2023-03-10 11:28:49 +0300

    Chunk exercise

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,12 +9,48 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-  let chunkedArray = [...array];
-  chunkedArray = chunkedArray.reduce((prev, curr, index, arr) => {
-    return prev;
-  });
+  let chunkedArray = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunkedArray.push(array.slice(index, index + size));
+    index += size;
+  }
 
   return chunkedArray;
 }
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   let chunkedArray = [];
+//   let newArr = [...array];
+
+//   array.forEach((el, i, arr) => {
+//     if ((i + 1) % size === 0) {
+//       chunkedArray.push(newArr.splice(0, size));
+//     }
+
+//     if (newArr.length !== 0 && newArr.length < size) {
+//       chunkedArray.push(newArr.splice(0, newArr.length));
+//     }
+//   });
+
+//   return chunkedArray;
+// }
+
+// function chunk(array, size) {
+//   let chunkedArray = [];
+
+//   for (let el of array) {
+//     let last = chunkedArray[chunkedArray.length - 1];
+
+//     if (!last || last.length === size) {
+//       chunkedArray.push([el]);
+//     } else {
+//       last.push(el);
+//     }
+//   }
+
+//   return chunkedArray;
+// }
