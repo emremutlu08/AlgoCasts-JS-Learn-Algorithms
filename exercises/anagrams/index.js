9b16e7b4515aa46ae9d15baf5b47d42569560660
emremutlu08 <mutluemre93@gmail.com>
2023-03-13 21:38:39 +0300

    Anagrams exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const charMapA = buildCharMap(stringA);
+  const charMapB = buildCharMap(stringB);
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+
+  for (let char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
+
+// function onlyStringLowerCase(str) {
+//   return str.replace(/[^\w]/g, "").toLowerCase();
+// }
+
+// function anagrams(stringA, stringB) {
+//   stringA = onlyStringLowerCase(stringA);
+//   stringB = onlyStringLowerCase(stringB);
+
+//   return (
+//     JSON.stringify(stringA.split("").sort()) ===
+//     JSON.stringify(stringB.split("").sort())
+//   );
+// }
 
 module.exports = anagrams;
