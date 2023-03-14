70aa3073007bb6262891e1ebe1254cd937b8ba7a
emremutlu08 <mutluemre93@gmail.com>
2023-03-14 15:41:13 +0300

    Capitalize exercise

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,51 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+// function capitalize(str) {
+//   const words = [];
+
+//   for (let word of str.split(" ")) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+
+//   return words.join(" ");
+// }
+
+// function capitalize(str) {
+//   // Let split the string by its spaces
+//   const strArray = str.split(" ");
+//   // We have something like this:
+//   // strArray = ["hello,", "world!", "i", "am", "a", "programmer"];
+
+//   // Then get the first letter of each element of array
+//   // Firstly mapping the array
+//   const capitalizeString = strArray
+//     .map((element) => {
+//       let splittedElement = element.split("");
+//       const uppercaseFirstCharOfElement = splittedElement[0].toUpperCase();
+//       splittedElement[0] = uppercaseFirstCharOfElement;
+
+//       const capitalizedElement = splittedElement.join("");
+
+//       return capitalizedElement;
+//     })
+//     .join(" ");
+
+//   return capitalizeString;
+// }
 
 module.exports = capitalize;
