2e721faccceebead6edecbc7837d3d1f2e59dd50
emremutlu08 <mutluemre93@gmail.com>
2023-03-14 21:39:28 +0300

    Vowels exercise

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,44 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // Regex
+  const matches = str.match(/[aeiou]/gi) || [];
+
+  return matches.length;
+}
+
+// function vowels(str) {
+//     let count = 0;
+//     const checker = ["a", "e", "i", "o", "u"];
+
+//     for (let char of str.toLowerCase()) {
+//       if (checker.includes(char)) {
+//         count++;
+//       }
+//     }
+
+//     return count;
+//   }
+
+// function vowels(str) {
+//   let vowelCount = 0;
+
+//   // Convert all string to lowerCase
+//   str = str.toLowerCase();
+
+//   str.split("").forEach((el) => {
+//     if (isCharVowel(el)) vowelCount++;
+//   });
+
+//   return vowelCount;
+// }
+
+// function isCharVowel(char) {
+//   // Vowels are the characters 'a', 'e', 'i', 'o', and 'u'.
+//   return (
+//     char === "a" || char === "e" || char === "i" || char === "o" || char === "u"
+//   ); // Boolean
+// }
 
 module.exports = vowels;
