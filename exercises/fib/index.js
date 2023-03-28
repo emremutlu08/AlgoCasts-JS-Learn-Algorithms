786cc7c085b31c2f27f2f01b20ac46a6acbaf009
emremutlu08 <mutluemre93@gmail.com>
2023-03-28 22:20:51 +0300

    Fib exercise

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,45 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {}; // Boş bir nesne tanımlanır, önbelleği temsil eder.
+  return function (...args) {
+    // Memoize edilmiş fonksiyon döndürülür.
+    if (cache[args]) {
+      // Eğer sonuç önbellekte bulunursa,
+      return cache[args]; // Önbellekten sonucu döndür.
+    }
+
+    const result = fn(...args); // Aksi halde, orijinal fonksiyon çağrılır.
+    cache[args] = result; // Sonucu önbelleğe kaydet.
+
+    return result; // Sonuçları döndürür.
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
+
+// function fib(n) {
+//   // Start from 0 and 1
+//   // Add prev two elements
+
+//   const fibonacci = [0, 1];
+
+//   while (fibonacci.length - 1 < n) {
+//     fibonacci.push(
+//       fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2]
+//     );
+//   }
+
+//   return fibonacci[n];
+// }
 
 module.exports = fib;
